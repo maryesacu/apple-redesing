04fcef6c099095cf162c484751e230fc6795d62b
marlon <maryesacu@gmail.com>
2022-11-15 21:29:26 -0500

    change: add differents headerLinks to Header component

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,26 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Header() {
-    return (
-        <header>
-            <div className="relative w-10 h-5">
-                <Image src="https://rb.gy/vsvv2o" fill objectFit="contain" alt=""/>
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <div className="flex items-center justify-center md:w-1/5">
+        <Link href="/">
+          <div className="opacity relative h-10 w-5 cursor-pointer transition hover:opacity-100">
+            <Image src="https://rb.gy/vsvv2o" width={14} height={16} alt="" />
+          </div>
+        </Link>
+      </div>
+      <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
+        <a className="headerLink">Product</a>
+        <a className="headerLink">Explore</a>
+        <a className="headerLink">Support</a>
+        <a className="headerLink">Business</a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
-
-/* function Header () {
-    return (
-        <header>
-            <div className="relative h-10 w-5">
-                <Image src="https://rb.gy/vsvv2o" layout="fill" objectFit="contain"/>
-            </div>
-        </header>
-    )
-}
-
-export default Header */
