e3313ae6eb597b5d53a2be1609d9a21a1e439af6
marlon <maryesacu@gmail.com>
2022-11-15 20:27:19 -0500

    change: modify Header Component

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
 import React from "react";
 
-const Header = () => {
+function Header() {
     return (
         <header>
-            <div className="relative h-10 w-5">
-                <Image  src="https://rb.gy/vsvv2o" layout="fill" objectFit="contain"/>
+            <div className="relative w-10 h-5">
+                <Image src="https://rb.gy/vsvv2o" fill objectFit="contain" alt=""/>
             </div>
         </header>
     )
 }
 
-export default Header
+export default Header;
+
+/* function Header () {
+    return (
+        <header>
+            <div className="relative h-10 w-5">
+                <Image src="https://rb.gy/vsvv2o" layout="fill" objectFit="contain"/>
+            </div>
+        </header>
+    )
+}
+
+export default Header */
